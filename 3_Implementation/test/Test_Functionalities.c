b77f4c35e371039ce4bdf93ee926aca16c82c1d5
manoharbravara <manohar.bravara@hotmail.com>
2021-04-15 09:32:26 +0530

    updating

---
+++ 3_Implementation/test/Test_Functionalities.c
@@ -0,0 +1,27 @@
+#include "unity.h"
+
+/* Modify these two lines according to the project */
+#include "Functionalities.h"
+#define PROJECT_NAME    "Train Ticket Reservation"
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+
+  
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Stepin_mini_project_LTTS
-This is project repo is for stepin mini project. All my learnings from the stepin programm used in this project
+# MiniProject
+Railway Ticket Reservation application written in C programming language.
